c791dd88cf8cf290a8630b6cda76e3f4652c56cb
abhishekparanthanate <142440592+abhishekparanthanate@users.noreply.github.com>
2023-11-14 23:05:14 -0800

    Add files via upload

--- MediaLib.java
+++ MediaLib.java
@@ -7,6 +7,14 @@ public class MediaLib
 {
   private Book book;
   private Movie movie;
+  private Song song;
+
+  private static String owner = "Me";
+
+  private static int numEntries;
+  private static int numBooks;
+  private static int numMovies;
+  private static int numSongs;
 
   public void addBook(Book b)
   {
@@ -18,25 +26,74 @@ public class MediaLib
     movie = m;
   }
 
-  public String toString() 
-  {
-    try{
-      String info = book.toString();
-      
-      try{
-        info = movie.toString();
-        return info;
-      } 
-      catch(Exception e){
-        return info;
-      }
-    } 
-    catch(Exception e){
-      
-      String info = "No info available.";
-      return info;
+  public void addSong(Song s) {
+    if (song == null) {
+      numEntries++;
+      numSongs++;
+      song = s;
+    } else {
+      System.out.println("cannot add more than one Song!");
     }
   }
+
+  public void changeSong(Song s) {
+    song = s;
+  }
+
+
+  public static String getOwner() {
+    return owner;
+  }
+
+  public static int getNumEntries() {
+    return numEntries;
+  } 
+
+  public static int getNumBooks() {
+    return numBooks;
+  }
+
+  public static int getNumMovies() {
+    return numMovies;
+  }
+
+  public static int getNumSongs() {
+    return numSongs;
+  }
+
+  public static void changeOwner(String o) {
+    owner = o;
+  }
+
+  
+
+  public String toString() 
+  {
+    String info = "book: " + book + "\nmovie: " + movie + "\nsong: " + song;
+    
+    return info;
+  }
+
+
+  // public String toString() 
+  // {
+  //   try{
+  //     String info = book.toString();
+      
+  //     try{
+  //       info = movie.toString();
+  //       return info;
+  //     } 
+  //     catch(Exception e){
+  //       return info;
+  //     }
+  //   } 
+  //   catch(Exception e){
+      
+  //     String info = "No info available.";
+  //     return info;
+  //   }
+  // }
   
   public void testBook(Book tester){
     tester.setTitle("Super duper tester title");
---
+++ Song.java
@@ -0,0 +1,66 @@
+public class Song {
+  private String title;
+  private String author;
+  private double rating;
+  private String date;
+  
+  /*** Constructor ****/
+  public Song(String t, String a)
+  {
+    title = t;
+    author = a;
+    rating = 0;
+    date = Date.getDate();
+  }
+
+  public Song(String t) {
+    title = t;
+    author = "";
+    rating = 0;
+    date = Date.getDate();
+  }
+
+  public boolean equals(Song b) {
+    if (title == b.title && author == b.author) {
+      return true;
+    }
+    return false;
+  }
+  
+   /*** Accessor methods ***/
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+  
+  public double getRating() {
+    return rating;
+  }
+  
+  public String toString() 
+  {
+    String info = "\"" + title + "\", written by " + author + " added at " + date;
+    if (rating != 0) 
+    { 
+      info += ", rating is " + rating;
+    }
+    return info;
+  }
+
+  /*** Mutator methods ***/
+  public void setTitle(String t) {
+    title = t;
+  }
+
+  public void setAuthor(String a) {
+    author = a;
+  }
+
+  public void adjustRating(int r) {
+    if (r < 0 || r > 10) { return; }
+    rating = r;
+  }
+}
